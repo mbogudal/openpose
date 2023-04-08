b559294ff3531e0066b183e7d8e4ec5e31b4bec4
Mikolaj Bogudal <mbogudal@gmail.com>
2023-04-08 14:37:30 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+conda install python=3.9 --yes
+conda install -c anaconda jupyter --yes
+conda install -c anaconda notebook --yes
+
+brew install caffe
+
+git clone git@github.com:mbogudal/openpose.git
+mkdir openpose/build
+
+#next steps
+# git submodule update --init --recursive
+# cmake-gui /opt/homebrew/Cellar/caffe /opt/homebrew/Cellar/caffe/1.0_43/lib/libcaffe.dylib
+# make -j$(sysctl -n hw.logicalcpu)
+# sudo make install
+# python3 -c "import site; print(site.getsitepackages()[0])"
+# sudo cp python/openpose/pyopenpose.cpython-39-darwin.so /path/to/site-packages
+# python -c "import pyopenpose as op; print('PyOpenPose imported successfully')"
